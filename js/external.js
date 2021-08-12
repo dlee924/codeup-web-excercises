6a4c277e6950edc412e56ef6c11c4dbd2f73bf82
darrell armstrong <67752984+dlee924@users.noreply.github.com>
2021-08-12 17:01:23 -0500

    first real excersie

--- js/external.js
+++ js/external.js
@@ -1,17 +1,35 @@
 console.log("hello from external javascript")
 
 alert("welcome to my website !")
+// alert window will pop up with message.//
+
 
 var userInput ;
 
-
-
-
 userInput = prompt("what is your favorite color my fellow human")
+
+// Alert window with prompt message will pop up//
 
 alert( userInput + "is my favorite color too")
 console.log( userInput, "is my fav color is green too")
 
+//user inputs into prompt and returns info. to computer//
 
 
 
+
+
+var littleMermaid = prompt("how many days have you had the littleMermaid ")
+
+alert(littleMermaid*3)
+
+
+var Brotherbear = prompt("how many days have you had Brotherbear")
+
+alert(Brotherbear*3)
+
+var Herc = prompt("how many days have you had the Herc ")
+
+alert(Herc*3)
+
+alert(littleMermaid*3+Brotherbear*3+Herc*3)
