35d788fbed0b4b29415349101eccd0e35c0de8c1
darrell armstrong <67752984+dlee924@users.noreply.github.com>
2021-08-18 18:49:31 -0500

    updated exercises-loops-updated-1-3

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,38 +1,38 @@
-// console.log('hello')
-//
-//
-// var i=1;
-//
-// function showMultiplicationTable(num) {
-//
-//     var num = parseInt(prompt('Enter a number: '));
-//
-//     for(var i = 1; i <= 10; i++) {
-//
-//         // multiply i with number
-//         var result = i * num;
-//
-//         // display the result
-//         console.log(`${num} * ${i} = ${result}`);
-//
-//
-//
-// }}
-//
-// showMultiplicationTable(3)
+console.log('hello')
+
+
+var i=1;
+
+function showMultiplicationTable(num) {
+
+    var num = parseInt(prompt('Enter a number: '));
+
+    for(var i = 1; i <= 10; i++) {
+
+        // multiply i with number
+        var result = i * num;
+
+        // display the result
+        console.log(`${num} * ${i} = ${result}`);
+
+
+
+}}
+
+showMultiplicationTable(3);
 
 
 
 // generating 10 random numbers Exercise//
 
 
-// let i = 10
 
-// for loop with var declaring the min and max values//
-for(var num=20; num<=200; num++){
+//for loop with var declaring the min and max values//
+
+for(var num=20; num<=200; num++) {
 
     //code creating random number
-     random =Math.floor(Math.random()*10 + 1)
+    random = Math.floor(Math.random() * 10 + 1)
 
 
     // send user message asking for random number.//
@@ -40,22 +40,18 @@ for(var num=20; num<=200; num++){
     var userNum = prompt("choose a random number between 20 and 200");
 
 
-
     // shows a message in console displaying if number is even or odd//
-    //  num%2==0? console.log(`${num} is even`):console.log(`${num} is odd`);
+    // num % 2 == 0 ? console.log(`${num} is even`) : console.log(`${num} is odd`);
 
-    if(userNum%2==0) {console.log(userNum+ "is even")
-     }
-     else{console.log(userNum+"is odd")};
+    if (userNum % 2 == 0 ) {
+        console.log(userNum + "is even")
+    } else {
+        console.log(userNum + "is odd")
+    }
+    ;
 
-
-
-    // stops for while loop from repeating prompt message//
 
 }
-
-
-///
 
 
 
