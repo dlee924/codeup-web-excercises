86f2ae18e272ef15c516b1f1e43e0270e8403b24
darrell armstrong <67752984+dlee924@users.noreply.github.com>
2021-08-18 19:57:49 -0500

    updated exercises-loops-updated-1-5

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,63 +1,70 @@
-var i=1;
-
-function showMultiplicationTable(num) {
-
-    var num = parseInt(prompt('Enter a number: '));
-
-    for(var i = 1; i <= 10; i++) {
-
-        // multiply i with number
-        var result = i * num;
-
-        // display the result
-        console.log(`${num} * ${i} = ${result}`);
-
-
-
-}}
-
-showMultiplicationTable(3);
-
-
-
-// generating 10 random numbers Exercise//
-
-
-
-//for loop with var declaring the min and max values//
-
-for(var num=20; num<=200; num++) {
-
-    //code creating random number
-    random = Math.floor(Math.random() * 10 + 1)
-
-
-    // send user message asking for random number.//
-
-    var userNum = prompt("choose a random number between 20 and 200");
-
-
-    // shows a message in console displaying if number is even or odd//
-
-    if (userNum % 2 == 0 ) {
-        console.log(userNum + "is even")
-    } else {
-        console.log(userNum + "is odd")
-    }
-    ;
-    break;
-
-
-}
-
-
-
-
+// var i=1;
+//
+// function showMultiplicationTable(num) {
+//
+//     var num = parseInt(prompt('Enter a number: '));
+//
+//     for(var i = 1; i <= 10; i++) {
+//
+//         // multiply i with number
+//         var result = i * num;
+//
+//         // display the result
+//         console.log(`${num} * ${i} = ${result}`);
+//
+//
+//
+// }}
+//
+// showMultiplicationTable(3);
+//
+//
+//
+// // generating 10 random numbers Exercise//
+//
+//
+//
+// //for loop with var declaring the min and max values//
+//
+// for(var num=20; num<=200; num++) {
+//
+//     //code creating random number
+//     random = Math.floor(Math.random() * 10 + 1)
+//
+//
+//     // send user message asking for random number.//
+//
+//     var userNum = prompt("choose a random number between 20 and 200");
+//
+//
+//     // shows a message in console displaying if number is even or odd//
+//
+//     if (userNum % 2 == 0 ) {
+//         console.log(userNum + "is even")
+//     } else {
+//         console.log(userNum + "is odd")
+//     }
+//     ;
+//     break;
+//
+//
+// }
+//
+//
+//
+//
+// //new problem//
+//
+//
+// for (var j = 1; j <= 9; j = j +1) {
+//     console.log(j.toString().repeat(j));}
+// }
 //new problem//
 
 
-for (var j = 1; j <= 9; j = j +1) {
-    console.log(j.toString().repeat(j));
+for (var i = 100; i >=1 ; i = i -5){console.log(i.toString())
+
+
 }
 
 
