aeb0dcb8c58efb652296e0089775347cc7463b6a
darrell armstrong <67752984+dlee924@users.noreply.github.com>
2021-08-19 09:09:00 -0500

    updated

--- js/while.js
+++ js/while.js
@@ -1,8 +1,25 @@
-console.log(("hello world this is a test"));
+// console.log(("hello world this is a test"));
+//
+//
+//
+// for (var a = 2; a <=65536; a = a * 2){
+//
+//     console.log(a.toString())
+// }
 
 
 
-for (var a = 2; a <=65536; a = a * 2){
+// do while loops//
 
-    console.log(a.toString())
-}
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+
+
+var clientsCones = Math.floor(Math.random() * 5) + 1;
+
+
+do {
+
+    if(allCones*clientsCones){console.log(allCones*clientsCones)}
+
+}while (allCones <100);
