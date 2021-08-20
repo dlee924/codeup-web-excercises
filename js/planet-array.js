3d5368cf9d8501cd1a6772e8a6c28e52a7117514
darrell armstrong <67752984+dlee924@users.noreply.github.com>
2021-08-20 15:58:52 -0500

    array exercises

---
+++ js/planet-array.js
@@ -0,0 +1,86 @@
+// (function(){
+//     "use strict";
+//
+//     var planets = [
+//         'Mercury',
+//         'Venus',
+//         'Earth',
+//         'Mars',
+//         'Jupiter',
+//         'Saturn',
+//         'Uranus',
+//         'Neptune'
+//     ];
+//
+//     /**
+//      * TODO:
+//      * Read each console log below, and write some javascript code to perform
+//      * the step that it describes
+//      */
+//
+//     console.log('Adding "The Sun" to the beginning of the planets array.');
+//     console.log(planets);
+//
+//     console.log('Adding "Pluto" to the end of the planets array.');
+//     console.log(planets);
+//
+//     console.log('Removing "The Sun" from the beginning of the planets array.');
+//     console.log(planets);
+//
+//     console.log('Removing "Pluto" from the end of the planets array.');
+//     console.log(planets);
+//
+//     console.log('Finding and logging the index of "Earth" in the planets array.');
+//
+//     console.log("Reversing the order of the planets array.");
+//     console.log(planets);
+//
+//     console.log("Sorting the planets array.");
+//     console.log(planets);
+// })();
+
+
+
+
+// var names = "darrell daryl andrew"
+//
+// var namesArray = names.split("darrell, daryl, andrew")
+//
+// console.log(namesArray)
+// //
+//
+//
+// var nameString = namesArray.join();
+//
+// console.log(nameString);
+
+
+var phoneNums = '2105556262 2105552121  2105558272'
+
+var cleanPhoneNums =phoneNums.split('2105556262, 2105552121 , 2105558272');
+
+console.log(cleanPhoneNums)
+
+
+
+// var phoneNums = '2105556262\n 2105552121\n  2105558272'
+//
+// function cleanNumbers(phoneString) {
+//
+//     var cleanNumbers = [];
+//
+//     //split phone numbers
+//
+//
+//     var cleanNumbers =phoneNums.split('\n');
+//
+//
+//
+// return cleanNumbers;
+
+
+
+
+
+
+
