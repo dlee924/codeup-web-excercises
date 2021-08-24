7bfd4c675a102d60ddec3541cc0f51eb79cf0241
darrell armstrong <67752984+dlee924@users.noreply.github.com>
2021-08-24 09:07:14 -0500

    objects exercise

--- lecture practices/objects lec.js
+++ lecture practices/objects lec.js
@@ -31,25 +31,31 @@ for (var i = 0; i < carType.features.length; i++ ){
 
 
 
+
+
+
+
+
+
+var vehicleOn = function () {
+    alert("Turning the car on" + this.make + this.model)
+
+}
+        carType.turnOn = vehicleOn;
+
+
 var spouseCar = {
-    color : "grey",
+    color: "grey",
 
     make: "honda",
 
     model: "civic",
 
-    engine: " 6 speed"
+    engine: " 6 speed",
 
-}
+    turnOn:  vehicleOn}
 
 
-
-var carOn = function () {
-    alert("Turning the car on" + this.make + this.model)
-
-}
-
-carType.turnOn = carOn;
 
 
 var ourGarage = [spouseCar , carType]
