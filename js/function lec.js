fc01196ef9405698342e0c8bb8bbb13650518a9c
darrell armstrong <67752984+dlee924@users.noreply.github.com>
2021-09-21 11:02:34 -0500

    updated exercises

--- js/function lec.js
+++ js/function lec.js
@@ -172,17 +172,25 @@
 
 
 
-function logsEven() {
 
 
-    var arr = [1, 2, 3, 4, 5, 6,7,8,9,10];
+function logsEven(number) {
 
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i] % 2 === 0) {
-            console.log((arr[i] ));;
-        }}}
 
-console.log(logsEven());
+    // var arr = [1, 2, 3, 4, 5, 6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+
+    // for (var i = 0; i < arr.length; i++) {
+    //     if (arr[i] % 2 === 0) {
+    //         console.log((arr[i] ));;
+    //     }}}
+
+// console.log(logsEven());
+
+
+for (var i = 0; i < number; i++) {
+    if (i % 2 === 0) {
+        console.log((i));;
+    }}}
 
 
 
